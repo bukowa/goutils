f0a0b30fb5b2c02114b515cc95fbb5c2f95c2d1b
Buk Bukowski <38087302+bukowa@users.noreply.github.com>
2020-08-15 21:46:01 +0200

    set -e

--- gofmt.sh
+++ gofmt.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+set -e
 echo "Running go fmt in $1"
 cd "$1" && go fmt ./...
--- gotest.sh
+++ gotest.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+set -e
 echo "Running go test in $1"
 cd "$1" && go test -v ./...
