da63c3b4863fd8cf0767193182df38b8a24c1544
Buk Bukowski <38087302+bukowa@users.noreply.github.com>
2020-08-15 22:02:18 +0200

    kubernetes client

--- kubernetes/config/flag/config.go
+++ kubernetes/config/flag/config.go
@@ -4,7 +4,7 @@ import (
 	"flag"
 	"github.com/bukowa/goutils/utils"
 	uflag "github.com/bukowa/goutils/utils/flag"
-	"k8s.io/client-go/rest"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"path/filepath"
 )
@@ -18,8 +18,12 @@ type ConfigFlags struct {
 	Path uflag.String
 }
 
-func (cf *ConfigFlags) NewClient() (*rest.Config, error) {
-	return clientcmd.BuildConfigFromFlags("", cf.Path.Value)
+func (cf *ConfigFlags) NewClient() (c *kubernetes.Clientset, err error) {
+	cfg, err := clientcmd.BuildConfigFromFlags("", cf.Path.Value)
+	if err != nil {
+		return
+	}
+	return kubernetes.NewForConfig(cfg)
 }
 
 var ConfigPathFlag = uflag.String{
