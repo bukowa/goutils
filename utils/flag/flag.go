058f07650112f7642bbff8275f9e4a2acc0c79cb
Buk Bukowski <38087302+bukowa@users.noreply.github.com>
2020-08-15 19:48:08 +0200

    use flagSet

--- utils/flag/flag.go
+++ utils/flag/flag.go
@@ -11,25 +11,44 @@ func ErrDefaultValueType(s string, t interface{}) error {
 }
 
 type Opts struct {
+	FlagSet *flag.FlagSet
 	Name    string
 	Default interface{}
 	Help    string
 }
 
+func NewOpts(name string, dfl interface{}, help string, sets ...*flag.FlagSet) *Opts {
+	var set *flag.FlagSet
+
+	if len(sets) == 0 {
+		set = flag.CommandLine
+	} else if len(sets) == 1 {
+		set = sets[0]
+	} else {
+		panic("Only one FlagSet can be passed!")
+	}
+	return &Opts{
+		FlagSet: set,
+		Name:    name,
+		Default: dfl,
+		Help:    help,
+	}
+}
+
 type String struct {
-	Opts
+	*Opts
 	Value string
 }
 
 type Bool struct {
-	Opts
+	*Opts
 	Value bool
 }
 
 func (s *String) Set() {
 	switch t := s.Opts.Default.(type) {
 	case string:
-		flag.StringVar(&s.Value, s.Name, t, s.Help)
+		s.FlagSet.StringVar(&s.Value, s.Name, t, s.Help)
 	default:
 		panic(ErrDefaultValueType("string", t))
 	}
@@ -38,7 +57,7 @@ func (s *String) Set() {
 func (b *Bool) Set() {
 	switch t := b.Opts.Default.(type) {
 	case bool:
-		flag.BoolVar(&b.Value, b.Name, t, b.Help)
+		b.FlagSet.BoolVar(&b.Value, b.Name, t, b.Help)
 	default:
 		panic(ErrDefaultValueType("bool", t))
 	}
--- utils/flag/flag_test.go
+++ utils/flag/flag_test.go
@@ -2,8 +2,8 @@ package flag
 
 import (
 	"flag"
-	"log"
 	"os"
+	"reflect"
 	"testing"
 )
 
@@ -11,23 +11,44 @@ func setArgs(args []string) {
 	os.Args = args
 }
 
+func TestNewOpts(t *testing.T) {
+	var flagSet = flag.NewFlagSet("test", flag.ExitOnError)
+	var opts = NewOpts("valid", true, "", flagSet)
+	if !reflect.DeepEqual(flagSet, opts.FlagSet){
+		t.Error(flagSet, opts.FlagSet)
+	}
+}
+
 func TestString_Set(t *testing.T) {
 	var baseArgs = os.Args
-	log.Println(baseArgs)
 	defer func() {
 		setArgs(baseArgs)
 	}()
 	os.Args = []string{baseArgs[0], "--paraasdddm=test"}
 	s := String{
-		Opts: Opts{
-			Name:    "paraasdddm",
-			Default: "",
-			Help:    "c",
-		},
+		Opts: NewOpts("paraasdddm", "", "c"),
 	}
 	s.Set()
 	flag.Parse()
 	if s.Value != "test" {
 		t.Error(s.Value)
+	}
+}
+
+func TestBool_Set(t *testing.T) {
+	var flagSet = flag.NewFlagSet("test", flag.ExitOnError)
+
+	b := Bool{
+		Opts: NewOpts("valid", false, "", flagSet),
+	}
+	if !reflect.DeepEqual(flagSet, b.FlagSet){
+
+	}
+	b.Set()
+	if err := flagSet.Parse([]string{"--valid=true"}); err != nil {
+		panic(err)
+	}
+	if b.Value != true {
+		t.Error(b.Value)
 	}
 }
